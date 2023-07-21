6b5b83019328f14fb850a6b74363c66750670640
ihsajjad <ihsajjad1@gmail.com>
2023-07-22 00:23:55 +0600

    added featured section

---
+++ src/pages/home/featured/Featured.jsx
@@ -0,0 +1,35 @@
+import SectionTitle from "../../../components/sectionTitle/SectionTitle";
+import featuredImg from "../../../assets/home/featured.jpg";
+import "./Featured.css";
+
+const Featured = () => {
+  return (
+    <section className="featured-section pt-8 text-white my-20">
+      <SectionTitle
+        subHeading="Check it out"
+        heading="Featured Item"
+      ></SectionTitle>
+
+      <div className="md:flex justify-center items-center md:gap-10 md:p-20">
+        <div>
+          <img src={featuredImg} alt="" />
+        </div>
+        <div>
+          <p>Aug 20, 2023</p>
+          <p className="uppercase">Where can i get some?</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas
+            obcaecati nobis esse recusandae adipisci nulla reprehenderit unde in
+            excepturi, repellendus enim accusantium a beatae ullam assumenda
+            vitae quaerat perspiciatis, itaque, sit nisi! Deserunt, accusamus
+            eveniet repellat dolor ipsa qui, magni numquam voluptas quas
+            repellendus tenetur. Quia tenetur ab dolorum provident!
+          </p>
+          <button className="btn btn-outline mt-6">Order Now</button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Featured;
--- src/pages/home/home/Home.jsx
+++ src/pages/home/home/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "../banner/Banner";
 import Category from "../category/Category";
+import Featured from "../featured/Featured";
 import PopularMenu from "../popularMenu/PopularMenu";
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
       <Banner />
       <Category />
       <PopularMenu />
+      <Featured />
     </div>
   );
 };
