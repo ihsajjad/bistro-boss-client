4db75f0315bdd55e60d43a119d253d95130c64a6
ihsajjad <ihsajjad1@gmail.com>
2023-07-21 20:56:24 +0600

    added navbar and banner in the home page

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../shared/footer/Footer";
+import NavBar from "../shared/navBar/NavBar";
 
 const Main = () => {
   return (
     <div>
+      <NavBar />
       <Outlet />
       <Footer />
     </div>
